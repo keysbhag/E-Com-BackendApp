c4a834df3a97308682a56eafd6c4e6f755338670
keyshawn bhagwandin <keyshawn.bhag@hotmail.com>
2022-10-23 12:35:05 -0400

    Finished routes for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -71,9 +71,27 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
-
+  try {
+    if (!req.body || !req.body.tag_name){
+      res.status(404).json(`Invalid entry of data`);
+      return;
+    }
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagData[0]) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    await ProductTag.destroy({ where: { tag_id: req.params.id } })
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(`Error from server ${err}`);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
